527d3b8f8114954916dfe0df67c8e1dd58def5b7
E. Gökhan Karahan <egkarahan@hotmail.com>
2021-05-27 00:54:01 +0300

    - IAuditable entity and its class are added - Auditable fields in BaseEntity class is removed.

--- egk.netcore-boilerplate.api/Data/Entities/Contracts/IEntity.cs
+++ egk.netcore-boilerplate.api/Data/Entities/Contracts/IEntity.cs
@@ -8,11 +8,6 @@ namespace egk.netcore_boilerplate.api.Data.Entities.Contracts
     public interface IEntity : IModifiableEntity
     {
         object Id { get; }
-        DateTime CreatedDate { get; set; }
-        DateTime? ModifiedDate { get; set; }
-        string CreatedBy { get; set; }
-        string ModifiedBy { get; set; }
-        byte[] Version { get; set; }
     }
 
     public interface IEntity<T> : IEntity
--- egk.netcore-boilerplate.api/Data/Entities/Entity.cs
+++ egk.netcore-boilerplate.api/Data/Entities/Entity.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace egk.netcore_boilerplate.api.Data.Entities
 {
-    public abstract class Entity<T> : IEntity<T>
+    public abstract class BaseEntity<T> : IEntity<T>
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -19,23 +19,8 @@ namespace egk.netcore_boilerplate.api.Data.Entities
         }
 
         public string Name { get; set; }
+        public string Description { get; set; }
 
-        private DateTime? createdDate;
-        [DataType(DataType.DateTime)]
-        public DateTime CreatedDate
-        {
-            get { return createdDate ?? DateTime.UtcNow; }
-            set { createdDate = value; }
-        }
-
-        [DataType(DataType.DateTime)]
-        public DateTime? ModifiedDate { get; set; }
-
-        public string CreatedBy { get; set; }
-
-        public string ModifiedBy { get; set; }
-
-        [Timestamp]
-        public byte[] Version { get; set; }
+     
     }
 }
